19f4ccffb445b33ce33882aa3a5ad5a4d27d2865
PrakashNarayanam <narayanamprakash@gmail.com>
2025-08-11 20:54:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,2 @@
 # MongoDB Atlas configuration
-import os
-
-MONGO_URI = os.environ.get('MONGO_URI', "mongodb+srv://requests:lM32Oc0n3Z0Q68oR@cluster0.xvb7a3a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+MONGO_URI = "mongodb+srv://requests:lM32Oc0n3Z0Q68oR@cluster0.xvb7a3a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
